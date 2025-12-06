117902abd3ba84e892e5c2437af6632909a8fc88
HwngMP <pmai4118@gmail.com>
2025-12-06 14:43:34 +0700

    Update GenerativeModel to use 'gemini-pro'

--- search_cake/main.py
+++ search_cake/main.py
@@ -21,7 +21,7 @@ if not GOOGLE_API_KEY:
     raise ValueError("Chưa cấu hình GOOGLE_API_KEY trên server!")
 
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 @app.get("/") # Hàm này để giảng viên bấm vào link check xem server sống hay chết
 def home():
